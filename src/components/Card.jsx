4cd70ad74a788b4036e084a60cd7b1460f72d7ad
Abhijeet <abhijeetgautam572@gmail.com>
2022-12-25 23:25:52 +0530

    updated Card.jsx and index.css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,25 @@
-import React from "react"
-import travelData from "../travelData"
+import React from "react";
+import pinIcon from "../assets/location-icon.png";
 
-export default function Card(props){
-    const data = props.data;
-    return (
-        <div className="card">
-
-            <div className="img-container">
-                <img className="location-img" src={data.img} alt="location" />
-            </div>
-            <div className="text-section">
-                Text-section of the Card
-            </div>
+export default function Card(props) {
+  const data = props.data;
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img className="location-img" src={data.img} alt="location" />
+      </div>
+      <div className="text-section">
+        <div className="location-container">
+          <img className="pin" src={pinIcon} alt="pin" />
+          <p className="country">{data.location.country}</p>
+          <a target="_blank" className="map-link" href={data.location.mapLink}>
+            View on Google Maps
+          </a>
         </div>
-    )
+        <h2>{data.location.name}</h2>
+        <p className="date">{data.year}</p>
+        <p className="description">{data.description}</p>
+      </div>
+    </div>
+  );
 }
--- src/travelData.js
+++ src/travelData.js
@@ -10,25 +10,64 @@
 */
 
 export default [
-    {
-        country:"India",
-        location: {
-            name:"India Gate",
-            mapLink:"https://google.com"
-        },
-        description:"lorem ipsum dolor sit amor",
-        img:"https://i.pinimg.com/564x/ac/94/35/ac9435b1dae07a86462fa6fa691498c4.jpg",
-        year:"2021"
+  {
+    location: {
+      country: "INDIA",
+      name: "The Taj Mahal",
+      mapLink:
+        "https://www.google.com/maps/search/Taj+Mahal/@27.1752105,77.9678235,12.81z",
     },
-    {
-        country:"India",
-        location: {
-            name:"India Gate",
-            mapLink:"https://google.com"
-        },
-        description:"lorem ipsum dolor sit amor",
-        img:"https://i.pinimg.com/564x/d9/31/e4/d931e46254d1d51b4743b43be8aefec4.jpg",
-        year:"2021"
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quasi, voluptatem saepe in rem adipisci laboriosam, accusantium laudantium impedit temporibus deserunt aspernatur! Corporis, omnis id? Eius tempore quaerat voluptates repudiandae! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quasi, voluptatem saepe in rem adipisci laboriosam!",
+    img: "https://i.pinimg.com/564x/ac/94/35/ac9435b1dae07a86462fa6fa691498c4.jpg",
+    year: "2022",
+  },
+  {
+    location: {
+      country: "INDIA",
+      name: "India Gate",
+      mapLink:
+        "https://www.google.com/maps/place/India+Gate/@28.6129167,77.227321,17z/data=!3m1!4b1!4m5!3m4!1s0x390ce2daa9eb4d0b:0x717971125923e5d!8m2!3d28.612912!4d77.2295097",
     },
-
-]
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quasi, voluptatem saepe in rem adipisci laboriosam, accusantium laudantium impedit temporibus deserunt aspernatur! Corporis, omnis id? Eius tempore quaerat voluptates repudiandae! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quasi, voluptatem saepe in rem adipisci laboriosam, accusantium laudantium ",
+    img: "https://i.pinimg.com/564x/d9/31/e4/d931e46254d1d51b4743b43be8aefec4.jpg",
+    year: "2021",
+  },
+  {
+    location: {
+      country: "INDIA",
+      name: "Nainital",
+      mapLink:
+        "https://www.google.com/maps/place/Nainital,+Uttarakhand/@29.3824865,79.4377611,14z/data=!3m1!4b1!4m5!3m4!1s0x39a0a1bc28fd9d61:0x7cae7ba916987db3!8m2!3d29.3919202!4d79.4542033",
+    },
+    description:
+      "Nainital is located in the Kumaon foothills of the Jagbeer Himalayas at a distance of 285 km (177 mi) from the state capital Dehradun and 345 km (214 mi) from New Delhi, the capital of India. Situated at an altitude of 1,938 metres (6,358 ft) above sea level, the city is set in a valley containing an eye-shaped lake, approximately two miles in circumference, and surrounded by mountains.",
+    img: "https://i.pinimg.com/474x/05/0f/88/050f88f23f8d52cfbbd153015a0cec7c.jpg",
+    year: "2017",
+  },
+  {
+    location: {
+      country: "INDIA",
+      name: "Manali",
+      mapLink:
+        "https://www.google.com/maps/place/Manali,+Himachal+Pradesh/@32.2395071,77.1696101,14z/data=!3m1!4b1!4m5!3m4!1s0x39048708163fd03f:0x8129a80ebe5076cd!8m2!3d32.2431872!4d77.1891761",
+    },
+    description:
+      "Manali is a town, near Kullu town in Kullu district in the Indian state of Himachal Pradesh.[2] It is situated in the northern end of the Kullu Valley, formed by the Beas River. The town is located in the Kullu district, approximately 270 kilometres (170 mi) north of the state capital of Shimla and 544 kilometres (338 mi) northeast of the national capital of New Delhi.",
+    img: "https://i.pinimg.com/474x/7a/2b/9a/7a2b9a190d5bf75553e983679ed3d0f5.jpg",
+    year: "2016",
+  },
+  {
+    location: {
+      country: "INDIA",
+      name: "Shimla",
+      mapLink:
+        "https://www.google.com/maps/place/Shimla,+Himachal+Pradesh/@31.0782882,77.1240015,13z/data=!3m1!4b1!4m5!3m4!1s0x390578e3e35d6e67:0x1f7e7ff6ff9f54b7!8m2!3d31.1048145!4d77.1734033",
+    },
+    description:
+      "Shimla is home to several buildings that are styled in the Tudorbethan and neo-Gothic architectures dating from the colonial era, as well as multiple temples and churches. The colonial architecture and churches, the temples, and the natural environment of the city attract tourists. Major city centre's attractions include the Shri Hanuman Jakhu (Statue), Jakhu Temple, Viceregal Lodge, Christ Church, Mall Road, The Ridge and Annadale.",
+    img: "https://i.pinimg.com/474x/11/11/a1/1111a19d6be160465f0f70f6be317b0b.jpg",
+    year: "2016",
+  },
+];
